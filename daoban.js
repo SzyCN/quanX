09bf66445e6b69d63f5b71f87c395d0119a38717
SzyCN <52116573+SzyCN@users.noreply.github.com>
2021-07-02 20:11:37 +0800

    Update daoban.js

--- daoban.js
+++ daoban.js
@@ -16,5 +16,5 @@
 // @/g                正则匹配时匹配多个key键值
 // ==/KeyExplain==
 
-var body = $response.body.replace(/"remainMonths": *,:'"remainMonths": 15').replace(/"remainDays": *,'"remainDays": 450')
+var body = $response.body.replace(/"2021-09-23"/g, "2022-09-23")
 $done({ body });
