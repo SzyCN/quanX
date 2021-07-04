14446302ad97149196ab4f725a3b0880fcf3b219
SzyCN <52116573+SzyCN@users.noreply.github.com>
2021-07-04 17:22:35 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -13,5 +13,5 @@ hostname = ap*.intsig.net
 ***************************
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1672502400"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"1656518400"}}};
 $done({body: JSON.stringify(obj)});
