71777822069478eb364896ef845c063394000fe6
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 07:07:42 +0800

    Update youtube ads.js

--- youtube ads.js
+++ youtube ads.js
@@ -1,17 +1,17 @@
 #!name=Unlock Youtube Premium
 #!desc=YouTube
 
-[URL Rewrite]
-https?:\/\/.+.googlevideo.com\/ptracking\?pltype=adhost, -reject
-https?:\/\/.+.youtube.com\/api\/stats\/ads, -reject
-https?:\/\/.+.youtube.com\/get_midroll_, -reject
-https?:\/\/.+.youtube.com\/pagead\/, -reject
-https?:\/\/.+.youtube.com\/ptracking\?, -reject
-https?:\/\/.+.youtube.com\/_get_ads, -reject
-https?:\/\/youtubei.googleapis.com\/.+ad_break, -reject
-https?:\/\/youtubei.googleapis.com\/youtubei\/.+(ad|log), -reject
-https?:\/\/s\.youtube\.com\/api\/stats\/qoe\?adformat, -reject
-https?:\/\/[\w-]+\.googlevideo\.com\/.+&oad, -reject -tinygif
+[Rule]
+URL-REGEX,https?:\/\/.+.googlevideo.com\/ptracking\?pltype=adhost,REJECT
+URL-REGEX,https?:\/\/.+.youtube.com\/api\/stats\/ads,REJECT
+URL-REGEX,https?:\/\/.+.youtube.com\/get_midroll_,REJECT
+URL-REGEX,https?:\/\/.+.youtube.com\/pagead\/,REJECT
+URL-REGEX,https?:\/\/.+.youtube.com\/ptracking\?,REJECT
+URL-REGEX,https?:\/\/.+.youtube.com\/_get_ads,REJECT
+URL-REGEX,https?:\/\/youtubei.googleapis.com\/.+ad_break,REJECT
+URL-REGEX,https?:\/\/youtubei.googleapis.com\/youtubei\/.+(ad|log),REJECT
+URL-REGEX,https?:\/\/s\.youtube\.com\/api\/stats\/qoe\?adformat,REJECT
+URL-REGEX,https?:\/\/[\w-]+\.googlevideo\.com\/.+&oad,REJECT-TINYGIF
 
 http-response ^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A), requires-body=1, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/youtube.js
 
