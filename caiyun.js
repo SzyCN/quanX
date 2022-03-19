99f677a1dc70563c286aa777534482a5f89b325a
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 00:59:38 +0800

    Create caiyun.js

---
+++ caiyun.js
@@ -0,0 +1,3 @@
+hostname = biz.caiyunapp.com
+
+http-response https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) requires-body=1, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/caiyunweather.js 
