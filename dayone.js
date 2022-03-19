ae1cf9c11adc572141bce78e46dc426eb47f377c
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 05:57:08 +0800

    Update dayone.js

--- dayone.js
+++ dayone.js
@@ -1,3 +1,3 @@
-hostname = dayone.* ，dayone.me
+hostname = dayone.*，dayone.me
   
 http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/dayone.js 
