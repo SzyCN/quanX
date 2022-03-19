d6821698cc74430cb7f4146f3776ced08d8ab6e8
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-19 22:57:06 +0800

    Create dayone.js

---
+++ dayone.js
@@ -0,0 +1,3 @@
+hostname = dayone.*
+  
+http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, script-path=https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/dayone.js 
