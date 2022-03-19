bdabb72036a85a3a654a356400be5970becc84dc
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-19 22:49:27 +0800

    Create baiduyunpan.js

---
+++ baiduyunpan.js
@@ -0,0 +1,3 @@
+hostname = pan.baidu.com
+
+http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/bdcloud.js
