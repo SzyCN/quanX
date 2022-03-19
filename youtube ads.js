1d887a14d0f366e7781dc270ec78c033924f3b86
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 06:56:49 +0800

    Update youtube ads.js

--- youtube ads.js
+++ youtube ads.js
@@ -2,16 +2,16 @@
 #!desc=YouTube
 
 [URL Rewrite]
-https?:\/\/.+.googlevideo.com\/ptracking\?pltype=adhost, REJECT
-https?:\/\/.+.youtube.com\/api\/stats\/ads, REJECT
-https?:\/\/.+.youtube.com\/get_midroll_, REJECT
-https?:\/\/.+.youtube.com\/pagead\/, REJECT
-https?:\/\/.+.youtube.com\/ptracking\?, REJECT
-https?:\/\/.+.youtube.com\/_get_ads, REJECT
-https?:\/\/youtubei.googleapis.com\/.+ad_break, REJECT
-https?:\/\/youtubei.googleapis.com\/youtubei\/.+(ad|log), REJECT
-https?:\/\/s\.youtube\.com\/api\/stats\/qoe\?adformat, REJECT
-https?:\/\/[\w-]+\.googlevideo\.com\/.+&oad, REJECT TINYGIF
+https?:\/\/.+.googlevideo.com\/ptracking\?pltype=adhost, -reject
+https?:\/\/.+.youtube.com\/api\/stats\/ads, -reject
+https?:\/\/.+.youtube.com\/get_midroll_, -reject
+https?:\/\/.+.youtube.com\/pagead\/, -reject
+https?:\/\/.+.youtube.com\/ptracking\?, -reject
+https?:\/\/.+.youtube.com\/_get_ads, -reject
+https?:\/\/youtubei.googleapis.com\/.+ad_break, -reject
+https?:\/\/youtubei.googleapis.com\/youtubei\/.+(ad|log), -reject
+https?:\/\/s\.youtube\.com\/api\/stats\/qoe\?adformat, -reject
+https?:\/\/[\w-]+\.googlevideo\.com\/.+&oad, -reject -tinygif
 
 http-response ^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A), requires-body=1, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/youtube.js
 
