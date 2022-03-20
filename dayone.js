c3743f3f018e69bab73a641f3c61d09fc48794be
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 20:41:05 +0800

    Update dayone.js

--- dayone.js
+++ dayone.js
@@ -1,4 +1,4 @@
-hostname=dayone.*
+hostname = dayone.*
 
 http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/dayone1.js
 
