4e6f556f8b6e70e996b91823278c8aec6ff9ae24
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 20:43:13 +0800

    Delete dayone.js

--- dayone.js
+++
@@ -1,4 +0,0 @@
-hostname = dayone.*
-
-http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/dayone1.js
-
