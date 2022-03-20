3cf7f22eeea4632f2fcb2dc5bd0673e3c819da02
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 20:14:47 +0800

    Delete dayone.js

--- dayone.js
+++
@@ -1,3 +0,0 @@
-hostname = dayone.*
-
-http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ requires-body=1 script-path=https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/dayone.js
