80bb7fb48a48197168f69566a4c4466e3b497b41
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 08:01:30 +0800

    Update dayone.js

--- dayone.js
+++ dayone.js
@@ -1,3 +1,3 @@
 hostname = dayone.me
   
-http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/dayone.js
+http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ script-path=https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/dayone.js
