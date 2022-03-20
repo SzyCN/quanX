ea6500affe30facc94cc2501b0dbc82f208dac34
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 20:44:11 +0800

    Create dayone.js

---
+++ dayone.js
@@ -0,0 +1,3 @@
+hostname = dayone.*
+
+http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/dayone1.js
