757242b573b3c7edb7955433c6ba44b5e57334da
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 20:33:56 +0800

    Update dayone.js

--- dayone.js
+++ dayone.js
@@ -1,4 +1,4 @@
 hostname=dayone.*
 
-http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, script-path=https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/dayone.js script-update-interval=0
+http-response ^https:\/\/dayone\.(me|app)\/api\/(users|v2\/users\/(account-status|receipt))$ requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/dayone.js
 
