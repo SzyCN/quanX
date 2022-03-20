6232db6a4018abd2c60a0b801b6655377f59df89
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-20 08:05:50 +0800

    Create dayone.js

---
+++ dayone.js
@@ -0,0 +1,3 @@
+hostname = dayone.me
+  
+http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ script-path=https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/dayone.js
