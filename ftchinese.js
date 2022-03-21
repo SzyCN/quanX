10e89856d308c56292129a0e8b93d1c7618509e9
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-21 09:33:50 +0800

    Create ftchinese.js

---
+++ ftchinese.js
@@ -0,0 +1,3 @@
+hostname = user.ftmailbox.cn
+
+http-response ^https?:\/\/user\.ftmailbox\.cn\/ios-receipt-validation\.php.* requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/ftchinese.js 
