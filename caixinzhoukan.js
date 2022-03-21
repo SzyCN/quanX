7119b5b5880ba48937dac170db0908d9cf185398
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-21 09:41:41 +0800

    Create caixinzhoukan.js

---
+++ caixinzhoukan.js
@@ -0,0 +1,3 @@
+hostname = ipadcms.caixin.com
+
+http-response http://ipadcms.caixin.com/tmp/articles/* requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/cxzk.js 
