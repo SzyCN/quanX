4015628f58d32fdd61b1bfd499f3968a21beb6f6
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-23 19:11:37 +0800

    Create Prisma.js

---
+++ Prisma.js
@@ -0,0 +1,3 @@
+hostname = api.neuralprisma.com
+
+http-response ^https:\/\/api\.neuralprisma\.com\/receipt\/ios\/status\/prisma\/.+ requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/Prisma.js
