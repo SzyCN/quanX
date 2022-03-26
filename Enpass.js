1129c9e136defecd597b60f2108e92556dec3c03
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-03-26 09:32:36 +0800

    Update Enpass.js

--- Enpass.js
+++ Enpass.js
@@ -1,3 +1,5 @@
 [rewrite_local]
-
 ^https:\/\/license\.enpass\.io/api/.+/subscription/me url script-echo-response https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/enpass.js
+
+[MITM]
+hostname=license.enpass.io
