3a7af56094f61587e0eefb2cdca7b56da4d8e93b
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-04-25 17:36:02 +0800

    Create duolinguo.js

---
+++ duolinguo.js
@@ -0,0 +1,3 @@
+hostname = duolingo-leaderboards-prod.duolingo.com
+
+http-response ^https:\/\/duolingo-leaderboards-prod\.duolingo\.com\/leaderboards* url script-response-body https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/duolingo.js
