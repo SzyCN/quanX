d6a03adb0c01cd7c238c2a9ca192c79d42133018
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-04-25 17:39:23 +0800

    Update duolinguo.js

--- duolinguo.js
+++ duolinguo.js
@@ -1,3 +1,3 @@
 hostname = duolingo-leaderboards-prod.duolingo.com
 
-http-response ^https:\/\/duolingo-leaderboards-prod\.duolingo\.com\/leaderboards* url script-response-body https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/duolingo.js
+http-response ^https:\/\/duolingo-leaderboards-prod\.duolingo\.com\/leaderboards* url script-path=https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/duolingo.js
