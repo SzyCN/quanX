e27f46b7badb8853fca29b195800e5012d85b45b
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-05-26 10:29:52 +0800

    Update kuwo_loon.js

--- kuwo_loon.js
+++ kuwo_loon.js
@@ -1,3 +1,3 @@
 hostname=*.kuwo.cn
 
-http-response ^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url requires-body=1 script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/kwyy.js
+http-response ^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url requires-body=1 https://raw.githubusercontent.com/SzyCN/quanX/main/kwyy.js
