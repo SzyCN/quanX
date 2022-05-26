60b34b494a5632e9d071d867f40a2cdc5bfa8e9e
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-05-26 10:27:20 +0800

    Update kuwo_loon.js

--- kuwo_loon.js
+++ kuwo_loon.js
@@ -1,11 +1,3 @@
-hostname=rich.kuwo.cn,*.kwcdn.kuwo.cn,mobilead.kuwo.cn, musicpay.kuwo.cn, vip1.kuwo.cn, audiobookpay.kuwo.cn, tingshu.kuwo.cn,omp-audiobookpay.lrts.me
+hostname=*.kuwo.cn
 
-
-
-http-response https?:\/\/omp-audiobookpay\.lrts\.me\/a\.p requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response ^https?:\/\/audiobookpay\.kuwo\.cn/a\.p requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response https://tingshu.kuwo.cn/v2/api/user/info requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response https?:\/\/audiobooks\.kuwo\.cn\/v2\/api\/user\/info requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response https?:\/\/tingshu\.kuwo\.cn\/v2\/api\/pay\/vip\/extraVipStatus requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response ^https?:\/\/vip1\.kuwo\.cn\/vip\/v2\/theme requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Script/kwyy.js
+http-response ^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url requires-body=1 script-path=https://raw.githubusercontent.com/SzyCN/quanX/main/kwyy.js
