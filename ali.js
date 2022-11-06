2c06636548e77a5f0b281a4597b70daae0ca0c3e
SzyCN <52116573+SzyCN@users.noreply.github.com>
2022-11-06 13:25:21 +0800

    Create ali.js

---
+++ ali.js
@@ -0,0 +1,2 @@
+hostname = *example.com
+http-response ^https?:\/\/.*\.example\.com url script-analyze-echo-response=1 script-path=https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Script/ali.js
