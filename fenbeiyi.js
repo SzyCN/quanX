f080f8ff4907a6581c0852e3c8eec0559aff3baa
SzyCN <52116573+SzyCN@users.noreply.github.com>
2023-03-03 19:55:03 +0800

    Create fenbeiyi.js

---
+++ fenbeiyi.js
@@ -0,0 +1,13 @@
+#!name=fenbeiyi
+#!desc=
+#!homepage=
+#!author=SzyCN
+#!icon=
+
+[Script]
+
+http-response ^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fenbeiyi.js, requires-body=true, timeout=10, tag=fenbeiyi
+
+[MITM]
+
+hostname = api.revenuecat.com
